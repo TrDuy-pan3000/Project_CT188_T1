52dc5e5c643e31daba5243a641f6af7b977f5044
Aholder-oss <nmltmvler@gmail.com>
2026-03-13 15:58:50 +0700

    detail page redesign

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,10 +1,24 @@
-const featuredImg = document.getElementById("main-img");
-const smallImgs = document.querySelectorAll(".small-img");
+// const featuredImg = document.getElementById("main-img");
+// const smallImgs = document.querySelectorAll(".small-img");
 
-smallImgs.forEach((img, index) => {
-  img.addEventListener("click", () => {
-    featuredImg.src = img.src;
-    smallImgs.forEach(i => i.classList.remove("sm-card"));
-    img.classList.add("sm-card");
+// smallImgs.forEach((img, index) => {
+//   img.addEventListener("click", () => {
+//     featuredImg.src = img.src;
+//     smallImgs.forEach(i => i.classList.remove("sm-card"));
+//     img.classList.add("sm-card");
+//   });
+// });
+
+const buttons = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const target = btn.dataset.tab;
+    buttons.forEach((b) => b.classList.remove("active"));
+    contents.forEach((p) => p.classList.remove("active"));
+    btn.classList.add("active");
+
+    document.getElementById(target).classList.add("active");
   });
 });
